4285e6299a9af78992b1a92478e57a1a63f8216e
Daniel-Kigozi <kdanalex69@gmail.com>
2023-07-26 11:32:36 +0300

    Add tests for controllers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
+  describe 'GET #index' do
+    before { get '/users' }
+
+    it 'returns a successful response' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('Find the list of users')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
+  describe 'GET #show' do
+    before { get '/users/1' }
+
+    it 'returns a successful response' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('Find user details')
     end
   end
 end
