f9feecfbb8f8bc3cdcd186500a8cc44e4a5e927b
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-08-11 10:02:14 +0200

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,5 +3,20 @@
 class Ability
   include CanCan::Ability
 
-  
+  def initialize(user)
+    user ||= User.new
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, Post
+      can :create, Post
+      can :create, Comment
+      can :update, Post, author_id: user.id
+      can :update, Comment, user_id: user.id
+      can :destroy, Post, author_id: user.id
+      can :destroy, Comment, user_id: user.id
+      can :index, Post
+    end
+  end
 end
